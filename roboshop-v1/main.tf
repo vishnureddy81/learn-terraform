bb7cce16da804f39c7279abc19c5af5ec62b7fab
vishnureddy <tarigopula34@gmail.com>
2025-07-16 22:09:54 +0530

    The route 53 and resources has been created.

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "fronted" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "frontend-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.fronted.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -14,6 +22,13 @@ resource "aws_instance" "mongodb" {
   tags = {
     name ="mongodb"
   }
+}
+resource "aws_route53_record" "mongodb" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "mongodb-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -24,6 +39,15 @@ resource "aws_instance" "catalogue" {
     name ="catalogue"
   }
 }
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "catalogue-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "redis" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -32,6 +56,15 @@ resource "aws_instance" "redis" {
     name ="redis"
   }
 }
+
+resource "aws_route53_record" "redis" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "redis-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -40,6 +73,14 @@ resource "aws_instance" "user" {
     name ="user"
   }
 }
+resource "aws_route53_record" "user" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "user-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -47,6 +88,13 @@ resource "aws_instance" "cart" {
   tags = {
     name ="cart"
   }
+}
+resource "aws_route53_record" "cart" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "cart-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -57,6 +105,15 @@ resource "aws_instance" "mysql" {
     name ="mysql"
   }
 }
+
+resource "aws_route53_record" "mysql" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "mysql-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -65,6 +122,14 @@ resource "aws_instance" "shipping" {
     name ="shipping"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "shipping-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
 resource "aws_instance" "rabbitmq" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -73,6 +138,14 @@ resource "aws_instance" "rabbitmq" {
     name ="rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "rabbitmq-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -81,6 +154,14 @@ resource "aws_instance" "payment" {
     name ="payment"
   }
 }
+resource "aws_route53_record" "payment" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "payment-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
 resource "aws_instance" "dispatch" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -88,4 +169,12 @@ resource "aws_instance" "dispatch" {
   tags = {
     name ="dispatch"
   }
+}
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = Z02665523CCE7X5KKVOH8
+  name    = "dispatch-dev.vishnureddy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.dispatch.private_ip]
 }

281aa7f745655aa7fa8663332084438ae6738cbe
vishnureddy <tarigopula34@gmail.com>
2025-07-16 17:03:14 +0530

    terraform resources has created.

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "fronted" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0fabcecd5236d6dce"]
+  tags = {
+    name ="frontend"
+  }
 }

90a534755b025332173caa0a78f62cbb0099ea0d
vishnureddy <tarigopula34@gmail.com>
2025-07-19 21:43:56 +0530

    variable is combination of some other string. Then we have use ${}

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -5,5 +5,9 @@ variable "URL" {
 ##Access the variable
 output "url" {
   value = "var.URL"
+}
 
+#variable is combination of some other string. Then we have use ${}
+output "URL1" {
+  value = "URL -${var.URL}"
 }

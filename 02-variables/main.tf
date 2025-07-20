f4d1f912213d0dc4cf3a460f5dcfac522c554c24
vishnureddy <tarigopula34@gmail.com>
2025-07-20 10:30:20 +0530

    variables

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -55,8 +55,11 @@ output "list_1" {
 }
 
 output "map1_aws_trainer" {
-  value = "var.map1"["aws"]["trainer"]
+  value = ["var.map1"]["aws"]["trainer"]
 }
 
 #declare a empty variable and we can get the value from CLI unlike shell and ansible we must and should declare a variable in order to use it.
 variable "trainer_name" {}
+output "trainer_name" {
+  value = var.trainer_name
+}

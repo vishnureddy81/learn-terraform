f2d045209733d6afb5683a0564a416f8a5d57c35
vishnureddy <tarigopula34@gmail.com>
2025-07-22 19:09:09 +0530

    data

--- 04-data/main.tf
+++ 04-data/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami      = "data.aws_ami.ami.id"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["data.aws_security.sg.id"]
+  vpc_security_group_ids= ["data.aws_security_group.sg.id"]
   tags = {
     name= "test"
 

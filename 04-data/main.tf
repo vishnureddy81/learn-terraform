754e202522b7f42707be08fbd9cf524cf9b9c8dc
vishnureddy <tarigopula34@gmail.com>
2025-07-22 19:23:42 +0530

    data

--- 04-data/main.tf
+++ 04-data/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "test" {
   }
 }
 
-data "aws_ami" "example"{
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "RHEL-9-DevOps-Practice"
   owners = ["973714476881"]

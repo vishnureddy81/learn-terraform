dee8f62674b177cb4745f92f1a2adb44208af49a
vishnureddy <tarigopula34@gmail.com>
2025-07-22 16:39:44 +0530

    The loops condition has been executed.

--- roboshop-count-loop/main.tf
+++ roboshop-count-loop/main.tf
@@ -1,4 +1,4 @@
-variable "instance" {
+variable "instances" {
   default = [
     "fronted",
     "mongodb",
@@ -16,20 +16,20 @@ variable "instance" {
 
 
 resource "aws_instance" "instance" {
-  count = length(var.instance)
+  count = length(var.instances)
   ami      = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
   tags = {
-    name= var.instance[count.index]
+    name= var.instances[count.index]
 
   }
 }
 
 resource "aws_route53_record" "record" {
-  count = length(var.instance)
+  count = length(var.instances)
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "${var.instance[count.index]}-dev.vishnuredddy1.online"
+  name    = "${var.instances[count.index]}-dev.vishnuredddy1.online"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.instance[count.index].private_ip]

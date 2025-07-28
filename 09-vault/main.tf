020e9624bb2c3b0607d47d47c1b9898dae3989ba
vishnureddy <tarigopula34@gmail.com>
2025-07-28 22:46:48 +0530

    vault

---
+++ 09-vault/main.tf
@@ -0,0 +1,28 @@
+provider "vault" {
+  address = "https://172.31.80.62:8200"
+  #vault private ip address we have to provide it.
+  token   = var.vault_token
+  skip_tls_verify = true
+}
+
+variable "vault_token" {}
+
+data "vault_generic_secret" "example" {
+  path = "test/my_credentials"
+}
+
+# data "vault_kv_secret_v2" "example" {
+#   name  = "my_credentials"
+#   mount = "test"
+# }
+
+resource "local_file" "foo" {
+  content  = data.vault_generic_secret.example.data["password"]
+  filename = "/tmp/secret"
+}
+
+# output "json" {
+#   value = data.vault_kv_secret_v2.example.data_json
+# }
+
+

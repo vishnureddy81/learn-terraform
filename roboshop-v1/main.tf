9dc37147999380018ecd9eebdc219178ad9de087
vishnureddy <tarigopula34@gmail.com>
2025-08-01 08:40:52 +0530

    vault in ansible

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,199 +1,170 @@
-resource "aws_instance" "fronted" {
+resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
   tags = {
-    name= "fronted"
+    Name = "frontend"
   }
 }
-# security group id means allow security group id
-resource "aws_route53_record" "fronted" {
+
+resource "aws_route53_record" "frontend" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "fronted-dev.vishnuredddy.online"
+  name    = "frontend-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
-  records = [aws_instance.fronted.private_ip]
+  ttl     = "30"
+  records = [aws_instance.frontend.private_ip]
 }
 
-# zone id means that's the dns zone id
 resource "aws_instance" "mongodb" {
-  ami      = "ami-09c813fb71547fc4f"
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
   tags = {
-    name= "mongodb"
-
+    Name = "mongodb"
   }
 }
-
 
 resource "aws_route53_record" "mongodb" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "mongodb-dev.vishnuredddy.online"
+  name    = "mongodb-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
+  ttl     = "30"
   records = [aws_instance.mongodb.private_ip]
 }
 
-
-
 resource "aws_instance" "catalogue" {
-  ami      = "ami-09c813fb71547fc4f"
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
   tags = {
-    name= "catalogue"
-
+    Name = "catalogue"
   }
 }
 
 resource "aws_route53_record" "catalogue" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "catalogue-dev.vishnuredddy.online"
+  name    = "catalogue-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
+  ttl     = "30"
   records = [aws_instance.catalogue.private_ip]
 }
 
-
 resource "aws_instance" "redis" {
-  ami      = "ami-09c813fb71547fc4f"
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
   tags = {
-    name= "redis"
-
+    Name = "redis"
   }
 }
 
 resource "aws_route53_record" "redis" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "redis-dev.vishnuredddy.online"
+  name    = "redis-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
+  ttl     = "30"
   records = [aws_instance.redis.private_ip]
 }
 
-
 resource "aws_instance" "user" {
-  ami      = "ami-09c813fb71547fc4f"
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
   tags = {
-    name= "user"
-
+    Name = "user"
   }
 }
 
 resource "aws_route53_record" "user" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "user-dev.vishnuredddy.online"
+  name    = "user-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
+  ttl     = "30"
   records = [aws_instance.user.private_ip]
 }
-resource "aws_instance" "cart" {
-  ami      = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
-  tags = {
-    name= "cart"
 
+resource "aws_instance" "cart" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "cart"
   }
 }
 
-
 resource "aws_route53_record" "cart" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "cart-dev.vishnuredddy.online"
+  name    = "cart-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
+  ttl     = "30"
   records = [aws_instance.cart.private_ip]
 }
 
-
 resource "aws_instance" "mysql" {
-  ami      = "ami-09c813fb71547fc4f"
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
   tags = {
-    name= "mysql"
-
+    Name = "mysql"
   }
 }
 
 resource "aws_route53_record" "mysql" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "mysql-dev.vishnuredddy.online"
+  name    = "mysql-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
+  ttl     = "30"
   records = [aws_instance.mysql.private_ip]
 }
 
 resource "aws_instance" "shipping" {
-  ami      = "ami-09c813fb71547fc4f"
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
   tags = {
-    name= "shipping"
-
+    Name = "shipping"
   }
 }
+
 resource "aws_route53_record" "shipping" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "shipping-dev.vishnuredddy.online"
+  name    = "shipping-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
+  ttl     = "30"
   records = [aws_instance.shipping.private_ip]
 }
-resource "aws_instance" "robbitmq" {
-  ami      = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
-  tags = {
-    name= "robbitmq"
 
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "rabbitmq"
   }
 }
 
 resource "aws_route53_record" "rabbitmq" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "robbitmq-dev.vishnuredddy.online"
+  name    = "rabbitmq-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
-  records = [aws_instance.robbitmq.private_ip]
+  ttl     = "30"
+  records = [aws_instance.rabbitmq.private_ip]
 }
 
-resource "aws_instance" "payment" {
-  ami      = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
-  tags = {
-    name= "payment"
 
+resource "aws_instance" "payment" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "payment"
   }
 }
 
 resource "aws_route53_record" "payment" {
   zone_id = "Z05470293UTKVXA4543KK"
-  name    = "payment-dev.vishnuredddy.online"
+  name    = "payment-dev.rdevopsb81.online"
   type    = "A"
-  ttl     = "300"
+  ttl     = "30"
   records = [aws_instance.payment.private_ip]
-}
-resource "aws_instance" "dispatch" {
-  ami      = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids= ["sg-0fabcecd5236d6dce"]
-  tags = {
-    name= "dispatch"
-
-  }
-}
-resource "aws_route53_record" "dispatch" {
-  zone_id = "Z05470293UTKVXA4543KK"
-  name    = "dispatch-dev.vishnuredddy.online"
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.dispatch.private_ip]
 }

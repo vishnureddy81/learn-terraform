f71bab5bdfa48172f64fc34707de9eb7e311eaea
vishnureddy <tarigopula34@gmail.com>
2025-08-16 13:15:40 +0530

    terraform modules

--- 08-mult-env-code-stracture/modules/ec2/main.tf
+++ 08-mult-env-code-stracture/modules/ec2/main.tf
@@ -1,10 +1,12 @@
 resource "aws_instance" "instance" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0fabcecd5236d6dce"]
+  vpc_security_group_ids = [""]
   tags = {
     Name = "test-${var.env}"
   }
 }
 
 variable "env" {}
+
+#

ff0e73957dc3c2975c5f78fedee8154a3b6e8b6e
vishnureddy <tarigopula34@gmail.com>
2025-08-16 12:15:25 +0530

    terraform modules

--- 06-modules/ec2/main.tf
+++ 06-modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  vpc_security_group_ids = ["sg-0fabcecd5236d6dce"]
   tags = {
     Name = var.instance_name
   }

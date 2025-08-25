b46b5296dd9f7d4c1fcb655e1e30e511d0fbcafd
vishnureddy <tarigopula34@gmail.com>
2025-08-25 17:46:02 +0530

    terraform roboshop project dev and qa env

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -2,14 +2,14 @@ provider "vault" {
   address = "https://172.31.14.180:8200"
   #vault private ip address we have to provide it.
   token   = var.vault_token
-  skip_tls_verify = true
+  # skip_tls_verify = true
 }
 
 variable "vault_token" {}
 
-data "vault_generic_secret" "example" {
-  path = "test/my_credentials"
-}
+# data "vault_generic_secret" "example" {
+#   path = "test/my_credentials"
+# }
 
 data "vault_kv_secret_v2" "example" {
   name  = "my_credentials"
@@ -17,7 +17,7 @@ data "vault_kv_secret_v2" "example" {
 }
 
 resource "local_file" "foo" {
-  content  = data.vault_generic_secret.example.data["password"]
+  content  = jsonencode(data.vault_kv_secret_v2.example.data_json)
   filename = "/tmp/secret"
 }
 

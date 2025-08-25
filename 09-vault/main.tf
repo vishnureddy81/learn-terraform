61a9da88c212c8ff8bd1626b56d09ef7fc6e1a28
vishnureddy <tarigopula34@gmail.com>
2025-08-25 18:01:54 +0530

    terraform roboshop project dev and qa env

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -7,17 +7,17 @@ provider "vault" {
 
 variable "vault_token" {}
 
-# data "vault_generic_secret" "example" {
-#   path = "test/my_credentials"
-# }
-
-data "vault_kv_secret_v2" "example" {
-  name  = "my_credentials"
-  mount = "test"
+data "vault_generic_secret" "example" {
+  path = "test/my_credentials"
 }
 
+# data "vault_kv_secret_v2" "example" {
+#   name  = "my_credentials"
+#   mount = "test"
+# }
+
 resource "local_file" "foo" {
-  content  = data.vault_kv_secret_v2.example.data_json["password"]
+  content  = data.vault_generic_secret.example.data_json["password"]
   filename = "/tmp/secret"
 }
 
